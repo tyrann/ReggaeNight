eef94a037a1f20c1aa4bc0a6592dd772dffdcf73
tyrann <thomas.j.rubeli@gmail.com>
2016-03-13 17:02:45 +0100

    Add music player

--- app/src/main/java/com/corporation/appollo/reggaenight/MainActivity.java
+++ app/src/main/java/com/corporation/appollo/reggaenight/MainActivity.java
@@ -1,13 +1,29 @@
 package com.corporation.appollo.reggaenight;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer mPlayer = MediaPlayer.create(this,R.raw.reggae_night);
+    }
+    private void playReggae() {
+        mPlayer.start();
+    }
+
+    private void stopReggae(){
+        mPlayer.stop();
+    }
+    public void onDestroy() {
+
+        mPlayer.stop();
+        super.onDestroy();
+
     }
 }
